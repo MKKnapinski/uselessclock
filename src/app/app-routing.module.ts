77b30fefae9ab4e4809164e84577ed6ba19fbe51
morti <morti@ncdc.pl>
2020-05-16 01:01:36 +0200

    some useless time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ClockComponent} from './clock/clock.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ClockComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-clock',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clock.component.scss']
 })
 export class ClockComponent implements OnInit {
+  uselessTime: string;
+  randomIntFromInterval = (min, max): number => Math.floor(Math.random() * (max - min + 1) + min);
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.getUselessTime(this.randomIntFromInterval(3, 3));
+  }
+
+
+  private getUselessTime(variant: number) {
+    switch (variant) {
+      case 1:
+        this.getUnixUselessTime();
+        break;
+      case 2:
+        this.getRandomFutureInfo();
+        break;
+      case 3:
+        this.getRandomPastInfo();
+        break;
+    }
+  }
+
+  private getUnixUselessTime() {
+    setInterval(() => {
+      this.uselessTime = Math.floor(new Date().getTime() / 1000) + ' seconds after 01.01.1970';
+    }, 1000);
+  }
+
+  private getRandomFutureInfo() {
+    const intoTheFuture = this.randomIntFromInterval(2, 59);
+    const futureTime = moment(new Date(), 'hh:mm:ss A')
+      .add(intoTheFuture, 'minutes')
+      .format('hh:mm:ss A');
+    this.uselessTime = _.join([intoTheFuture, 'minutes untill', futureTime], ' ');
+  }
+
+  private getRandomPastInfo() {
+    const intoThePast = this.randomIntFromInterval(2, 59);
+    const pastTime = moment(new Date(), 'hh:mm:ss A')
+      .subtract(intoThePast, 'minutes')
+      .format('hh:mm:ss A');
+    this.uselessTime = _.join([intoThePast, 'minutes ago was', pastTime], ' ');
   }
 
 }
